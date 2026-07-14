67994394b22ec2e27adc35a84791a0ee368c7028
milimontu81-beep <milimontu81@gmail.com>
2026-07-14 21:30:02 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,18 @@
 #wellcome massage 
 print("welcome to rule based chatbot ")
+
+#for greeting the user according to the time 
+import datetime 
+import time
+presenttime=time.localtime().tm_hour
+#for user name
+name=input("Enter your name:")
+#putting condition for greeting
+if 1<=presenttime<12:
+ print("Good morning",name)
+elif 12<=presenttime<17:
+ print("Good afternoon",name)
+elif 17<=presenttime<20:
+ print("Good evening",name) 
+else:
+ print("Good night",name) 
