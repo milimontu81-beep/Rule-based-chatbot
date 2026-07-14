762cc73f0fe97c5ee741059bc093d8cfad031eb5
milimontu81-beep <milimontu81@gmail.com>
2026-07-14 21:31:09 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -12,6 +12,11 @@ if 1<=presenttime<12:
  print("Good morning",name)
 elif 12<=presenttime<17:
  print("Good afternoon",name)
+
+#for chatbot responses
+print(f"Hello {name} I am your chatbot,ask me something ")
+#creating dictionary storing information for response 
+responses={"hi":"Hello "+name,"how are you":"I am fine","who are you":"I am your chatbot","who build you":"A developer name Montu mili build me","what is python":"python is a programming language","thankyou":"welcome,glad to hear that","do you have emotion":"No i don't have emotions"}
 elif 17<=presenttime<20:
  print("Good evening",name) 
 else:
